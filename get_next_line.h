8d1ad2d340874f033c22cd6e86baf37863e4b5d2
HIROKIASANO99 <asanohiroki0722@yahoo.co.jp>
2024-06-10 22:32:45 +0900

    ippome

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: hiasano <hiasano@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 20:04:20 by hiasano           #+#    #+#             */
-/*   Updated: 2024/06/10 20:12:29 by hiasano          ###   ########.fr       */
+/*   Updated: 2024/06/10 22:17:59 by hiasano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H 
 # define GET_NEXT_LINE_H
 
-
+#include <stdlib.h>
+#include <fcntl.h> 
+#include <unistd.h>
 
 #endif
