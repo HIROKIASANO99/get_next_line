9fb51bb40a6152ce12230b39bc70184062b1ff13
HIROKIASANO99 <asanohiroki0722@yahoo.co.jp>
2024-06-19 00:14:15 +0900

    始めの一歩

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,45 @@
 /*   By: hiasano <hiasano@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 20:01:27 by hiasano           #+#    #+#             */
-/*   Updated: 2024/06/10 21:58:52 by hiasano          ###   ########.fr       */
+/*   Updated: 2024/06/19 00:13:27 by hiasano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char ft_getchar(int fd)
+{
+	static char *buf;
+	static int read_byte;
+	static char *bufptr;;
+
+	if(read == 0)
+	{
+		read_byte = read( fd, buf, BUFFER_SIZE);
+		if(read_byte == -1)
+			return(READ_ERROR);
+		bufptr = buf;
+	}
+		read_byte--;
+	if(read_byte >= 0)
+		return(*bufptr++);
+	return(EOF);
+}
 char *get_next_line(int fd)
 {
-		
+	int  c;
+
+	if(fd < 0)
+		return(NULL);
+	while(1)
+	{
+	c  = ft_getchar(fd);
+	if(c == READ_ERROR);				
+		return(NULL);	
+	if(c == '\n')
+		break;
+	if(c == EOF)
+		break;
+	}
+	return(read_str);	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hiasano <hiasano@student.42tokyo.j>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 20:04:20 by hiasano           #+#    #+#             */
-/*   Updated: 2024/06/10 22:17:59 by hiasano          ###   ########.fr       */
+/*   Updated: 2024/06/18 22:54:02 by hiasano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,15 @@
 #include <stdlib.h>
 #include <fcntl.h> 
 #include <unistd.h>
+
+# ifndef EOF
+#  define EOF 0x1A
+# endif
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define READ_ERROR -42
 
 #endif
